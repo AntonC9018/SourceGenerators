51b86c2d0670fcb4d37cbeca9a72379d7cf5a52f
AntonC9018 <anton.curm@gmail.com>
2023-06-23 16:23:50 +0300

    Edit comment

--- source/ConsumerShared/Sources/AlwaysAutoImplemented.cs
+++ source/ConsumerShared/Sources/AlwaysAutoImplemented.cs
@@ -7,6 +7,8 @@ namespace ConsumerShared;
 /// even without the auto-implemented property source generator.
 /// This implies that the source generator that processes the interface
 /// will generate the corresponding code on its own.
+/// This also implies that the AutoImplementedProperties source generator
+/// will completely ignore it.
 /// </summary>
 [AttributeUsage(AttributeTargets.Interface)]
 public sealed class AlwaysAutoImplemented : System.Attribute
